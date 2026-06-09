3a952b9d1d2f351ee3471013c03e5a9c6aac21c5
sagarjr11 <165011272+sagarjr11@users.noreply.github.com>
2026-06-09 10:27:58 +0000

    added localhost for codespaces in vite,config

--- E-commerce/frontend/vite.conf.js
+++ E-commerce/frontend/vite.conf.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: '0.0.0.0', port: 3000,
-    proxy: { '/api': { target: 'http://backend:5000', changeOrigin: true } }
+    proxy: { '/api': { target: 'http://localhost:5000', changeOrigin: true } }
   }
 })
